474d0ca4e7830b5b49d9ddf3fb1bc541a19a5950
Ian Blake <ian@ianbl8.com>
2023-05-15 18:15:38 +0100

    Use uuid module

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,8 @@
   let todoItems = [];
   let doneItems = [];
 
-  function uuidv4() {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-      var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    }); 
-  }
-
+  import { v4 as uuidv4 } from 'uuid';
+ 
   function addTodo() {
     console.log(todoText)
     const todo = {
