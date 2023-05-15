32b9f8291910827065349dc57a3e90e409d72fcc
Ian Blake <ian@ianbl8.com>
2023-05-15 21:17:59 +0100

    Component created for AddTodoForm

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script>
+  import { v4 as uuidv4 } from 'uuid';
+  import Title from "./Title.svelte";
+  import Todolist from './Todolist.svelte';
+  import AddTodoForm from './AddTodoForm.svelte';
+ 
   let todoText = "";
   let todoItems = [];
   let doneItems = [];
 
-  import { v4 as uuidv4 } from 'uuid';
-  import Title from "./Title.svelte";
-  import Todolist from './Todolist.svelte';
- 
-  function addTodo() {
-    console.log(todoText)
+  function addTodo(todoText) {
     const todo = {
       text: todoText,
       date: new Date().toLocaleString("en-IE"),
@@ -30,23 +30,10 @@
 </script>
 
 <div class="container">
-  <Title title="Simple Todo List" subTitle="Fun Things to do"/>
+  <Title title="Simple Todo List" subtitle="Fun Things to do"/>1
   <div class="section box">
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label for="todo" class="label">What should I do?</label>
-      </div>
-      <div class="field-body">
-        <div class="field">
-          <p class="control">
-            <input bind:value={todoText} id="todo" class="input" type="text" placeholder="Type something...">
-          </p>
-        </div>
-        <button on:click={addTodo} class="button">Add Todo</button>
-      </div>
-    </div>
+    <AddTodoForm addTodo="{addTodo}" />
   </div>
   <Todolist caption="Tasks to do" items="{todoItems}" deleteHandler="{deleteTodo}"/>
   <Todolist caption="Tasks done" items="{doneItems}"/>
 </div>
-
